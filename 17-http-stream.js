6adfe945e369b97276c256d61fa784a0752dec56
David Huang <david.huang3214@gmail.com>
2023-05-08 11:23:43 -0700

    5/8/2023

--- 17-http-stream.js
+++ 17-http-stream.js
@@ -3,11 +3,8 @@ var fs = require('fs')
 
 http
     .createServer(function (req, res) {
-        // Don't send as one big file
         // const text = fs.readFileSync('./content/big.txt', 'utf8')
         // res.end(text)
-
-        // Send file in chunks with a filestream
         const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
         fileStream.on('open', () => {
             fileStream.pipe(res)
